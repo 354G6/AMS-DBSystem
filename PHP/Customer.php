5c5997fb080906e09abb4a6f957c281cdb075d8e
njwalsh <nickjameswalsh1@gmail.com>
2015-03-29 13:38:21 -0700

    Added register and login functions to Customer.php

--- PHP/Customer.php
+++ PHP/Customer.php
@@ -38,4 +38,34 @@
 		Close($sql);
 		return $table;
 	}
+	
+	//calls CustomerInsert() but hashes password first
+	function CustomerRegister($cid, $password, $name, $address, $phone){
+		$cost = 10;
+		$salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
+		$salt = sprintf("$2a$%02d$", $cost) . $salt;
+		$hash = crypt($password, $salt);
+		return CustomerInsert(CustomerRegister($cid, $hash, $name, $address, $phone));
+	}
+	
+	function CustomerLogin($cid, $password){
+		$r = 2;
+		$sql = Connect();
+		if ($sql->connect_error) {
+			return 1;
+		}
+		//this is a stupid way to retrieve hash, fix this
+		$stmt = $sql->prepare("SELECT password FROM Customer WHERE cid = '$cid'");
+		$stmt->execute();
+		$stmt->bind_result($hash);
+		if($stmt->fetch()){
+			if(hash_equals($hash, crypt($password, $hash))){
+				$r = 0;
+			}else{
+				$r = 2;
+			}
+		}
+		Close($sql);
+		return $r;
+	}
 ?>
