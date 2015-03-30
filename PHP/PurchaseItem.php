ea58c7b2a04623eb2e711fc435e47df2e9120220
njwalsh <nickjameswalsh1@gmail.com>
2015-03-29 17:52:34 -0700

    added DailyReport function to PurchaseItem.php

--- PHP/PurchaseItem.php
+++ PHP/PurchaseItem.php
@@ -38,4 +38,32 @@
 		Close($sql);
 		return $table;
 	}
+	
+	//returns table rows with report info and the total price in last element of array
+	function DailyReport($date){
+		$sql = Connect();
+		if ($sql->connect_error) {
+			return 1;
+		}
+		//$date format must be yyyy-mm-dd
+		$result = $sql->query("SELECT upc, category, SUM(price), SUM(quantity) 
+							   FROM Item 
+							   INNER JOIN PurchaseItem 
+							   ON Item.upc=PurchaseItem.upc 
+							   INNER JOIN Order 
+							   ON PurchaseItem.receiptId=Order.receiptId
+							   WHERE DATE(`date`) = '$date';
+							   GROUP BY upc");
+		$table = array()
+		while($row = mysqli_fetch_array($result)){
+			$table[] = $row;
+		}
+		Close($sql);
+		$totalPrice = 0;
+		for($i = 0; $i < count($table); $i++){
+			$totalPrice = $totalPrice + $table[i][2];
+		}
+		$table[] = $totalPrice;
+		return $table;
+	}
 ?>
