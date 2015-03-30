46a06b1df2472785ced03b6f7c697d232450ce50
Leon Lin <xy11235@gmail.com>
2015-03-29 22:12:11 -0700

    a
    a

--- testapp/testing.php
+++ testapp/testing.php
@@ -1,9 +1,3 @@
-// Using PDO_MySQL (connecting from App Engine)
-$db = new pdo('mysql:unix_socket=/cloudsql/ams-cmpt354g6-2015spring:test'),
-  'root',  // xy11235@gmail.com
-  ''       // A.H.Studenmund1009
-);
-
 // Using mysqli (connecting from App Engine)
 $sql = new mysqli(
   null, // host
@@ -12,10 +6,4 @@ $sql = new mysqli(
   '', // database name
   null,
   '/cloudsql/ams-cmpt354g6-2015spring:test'
-  );
-
-// Using MySQL API (connecting from APp Engine)
-$conn = mysql_connect(':/cloudsql/ams-cmpt354g6-2015spring:test',
-  'root', // xy11235@gmail.com
-  ''      // 
   );
