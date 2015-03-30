c1b972eb6370c4bf573e9895fd6833b639b77e93
Leon Lin <xy11235@gmail.com>
2015-03-29 22:10:12 -0700

    ff
    ff

--- testapp/testing.php
+++ testapp/testing.php
@@ -1,4 +1,21 @@
-<h1>ttesting</h1>
-<?php
-    echo "ttesting!";
-	echo "ttestingttesting";
+// Using PDO_MySQL (connecting from App Engine)
+$db = new pdo('mysql:unix_socket=/cloudsql/ams-cmpt354g6-2015spring:test'),
+  'root',  // xy11235@gmail.com
+  ''       // A.H.Studenmund1009
+);
+
+// Using mysqli (connecting from App Engine)
+$sql = new mysqli(
+  null, // host
+  'root', // xy11235@gmail.com
+  '',     // A.H.Studenmund1009
+  '', // database name
+  null,
+  '/cloudsql/ams-cmpt354g6-2015spring:test'
+  );
+
+// Using MySQL API (connecting from APp Engine)
+$conn = mysql_connect(':/cloudsql/ams-cmpt354g6-2015spring:test',
+  'root', // xy11235@gmail.com
+  ''      // A.H.Studenmund1009
+  );
