f7356f35e787ecff2beb80e84f1f244c295edfa0
Leon Lin <xy11235@gmail.com>
2015-04-02 21:24:41 -0700

    Create Delivery.php

---
+++ PHP/Delivery.php
@@ -0,0 +1,21 @@
+<?php
+	include_once 'Connect.php';
+	
+	function Delivery($receiptid, $DeliveryDate){
+	  $sql = Connect();
+	  if($sql->connect_error){
+	    return 1;
+	  }
+	  $result = $sql->query("SELECT deliveredDate FROM Order WHERE receiptid = '$receiptid'");
+	  
+	  if($result->num_rows !== 0){
+			$row = $result->fetch_row();
+			$result->close();
+			$sql->query("UPDATE Order SET deliveredDate = '$DeliveryDate' + '$row[6]' WHERE receiptid = '$receiptid'");
+		}else{
+			//receiptid is not in Order table
+			return 2;
+		}
+	  
+	}
+?>
