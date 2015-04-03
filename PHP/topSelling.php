2759e20b9a258ececeed243da59daaa067d4c8dc
Leon Lin <xy11235@gmail.com>
2015-04-02 22:25:17 -0700

    Update topSelling.php

--- PHP/topSelling.php
+++ PHP/topSelling.php
@@ -1,3 +1,29 @@
+<?php
+	include_once 'Connect.php';
+	
+	function topSelling($date, $topNum){
+		$sql = Connect();
+		if ($sql->connect_error) {
+			return 1;
+		}
+		
+		$result = $sql->query("SELECT TOP $TopNum P.upc, I.title, I.company, I.stock, sum(P.quantity) AS 'Quantities Sold', 
+                           FROM PurchaseItem P, Order O
+                           INNER JOINT Item I
+                           WHERE P.receiptid=O.receiptid, O.date='$date'
+                           GROUP BY P.upc
+                           ORDER BY sum(P.quantity) DESC");
+		
+		$table = array();
+		
+		while($row = mysqli_fetch_array($result)){
+			$table[] = $row;
+		}
+		
+		Close($sql);
+		return $table;
+	
+?>
 
 
 
@@ -5,8 +31,3 @@
 
 
 
-SELECT TOP $TopNum P.upc, sum(P.quantity) AS 'Quantities Sold'
-FROM PurchaseItem P, Order O
-WHERE P.receiptid=O.receiptid, O.date='$date'
-GROUP BY P.upc
-ORDER BY sum(P.quantity) DESC
