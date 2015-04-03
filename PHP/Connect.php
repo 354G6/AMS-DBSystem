ccd1b66686783b86381b9ed5df713b9c6facb02f
njwalsh <nickjameswalsh1@gmail.com>
2015-04-02 22:50:32 -0700

    modified connect function to automatically connect from app engine or dev server

--- PHP/Connect.php
+++ PHP/Connect.php
@@ -1,11 +1,13 @@
 <?php
 	function Connect(){
-		$sql = new mysqli('[2001:4860:4864:1:3208:ce09:5af3:c0cd]:3306',
-		'nicholas',
-		'amspassword',
-		'AMS_CMPT354'
-		);
-		
+		$sql = null;
+		if (isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
+			// Connect from App Engine.
+			$sql = new mysqli(null, 'root', '', 'amspassword', null, '/cloudsql/ams-cmpt354g6-2015spring:test');
+		} else {
+			// Connect from a development environment.
+			$sql = new mysqli('[2001:4860:4864:1:3208:ce09:5af3:c0cd]:3306', 'nicholas', 'amspassword', 'AMS_CMPT354');
+		}
 		return $sql;
 	}
 	
