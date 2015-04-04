560249664b75ee441fea2862d4f0c5c59cbff123
rickywongwk <rickywong2323@gmail.com>
2015-04-03 21:18:59 -0700

    deleted feedbackX.php
    feedback page not needed

--- UI/manager-defaultAdd.php
+++ UI/manager-defaultAdd.php
@@ -1,8 +1,59 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $validated=true;
+    $cid = $name = $password = $cpw = $address = $phone = "";
+    
+    //filter input data
+    $cid = filter($_POST["cid"]);
+    $name = filter($_POST["name"]);
+    $password = $_POST["password"];
+    $cpw = $_POST["cpw"];
+    $address = filter($_POST["address"]);
+    $phone = filter($_POST["phone"]);
+  
+    //validate
+    if ($cid==""||$password==""||$cpw==""||$password!=$cpw) {
+        $validated=false;
+    }
+    
+    if ($validated) {
+        //clear form data
+        $_POST["cid"]="";
+        $_POST["name"]="";
+        $_POST["address"]="";
+        $_POST["phone"]="";
+        
+        //include dirname(__FILE__) . "core/Customer.php";
+        $returnMessage="";
+        $result = 0; //= CustomerRegister($cid, $password, $name, $address, $phone)
+        if (result == 0) {
+            $returnMessage='Registered successfully!<br>Please <a href="?op=login">>>Click Here to Log In<<</a>';
+            //echo '<script>alert("Registered successfully!\nPlease Log In using your ID and Password.");</script>';
+            //echo '<script>window.location = "?op=login"</script>';
+        } else {
+            $errorMessage = array( '',
+                                'Unable to connect to the database.',
+                                'Failed executing query.' //how to get message like "Login ID already exists"?
+                            );
+            $returnMessage = 'Error:'.$errorMessage[$result];
+        }
+    }
+}
+
+function filter($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+
 <div class="entryBox">
 <h3>Add Item</h3>
 <form action="?procAddItem" method="POST">
-    <div class="textEntry">Item UPC: <input type="text" name="itemUPC" id="itemUPC"/></div>
-    <div class="textEntry">Quantity: <input type="text" name="addQuant" id="addQuant"/></div>
-    <div class="textEntry">Price: <input type="text" name="addPrice" id="addPrice"/></div>
+    <div class="textEntry">Item UPC: <input type="number" name="itemUPC" id="itemUPC" required/></div>
+    <div class="textEntry">Quantity: <input type="number" name="addQuant" id="addQuant" required/></div>
+    <div class="textEntry">Price: <input type="text" name="addPrice" id="addPrice" required/></div>
 </form>
 </div>
