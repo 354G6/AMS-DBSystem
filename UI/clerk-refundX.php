02e14cc76c4791a97a2f1660f021d52ec273b84d
laialanl <lialanl4@gmail.com>
2015-04-03 22:19:21 -0700

    Update clerk-refundX.php

--- UI/clerk-refundX.php
+++ UI/clerk-refundX.php
@@ -1,33 +1,41 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Refund Search</title>
-<link rel="stylesheet" type="text/css" href="stylesheets/project.css" />
-</head>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$validated=true;
+	$cid = $name = $password = $cpw = $address = $phone = "";
 
-<body>
-<h1>&nbsp;</h1>
-<div id="Header">
-  <h1>AMS DBSystem CMPT 354 G6</h1>
-  <h3 align="right">Home &nbsp; &nbsp; <a href="login.htm">Logout</a> </h3>
-</div>
+	//filter input data
+	$cid = filter($_POST["cid"]);
+	$name = filter($_POST["name"]);
+	$password = $_POST["password"];
+	
+	//validate
+	if ($cid==""||$password==""||$cpw==""||$password!=$cpw) {
+		$validated=false;
+	}
+	if ($validated) {
+		//clear form data
+		$_POST["cid"]="";
+		$_POST["name"]="";
+		$_POST["address"]="";
+	} else {
+		
+	}
+}
 
-<div id="Menu">
-  <h3>Item Menu</h3>
-  <p>
-  <ul id = "Navi" class = "Menu">
-  <li>DVD</li>
-  <li>CD</li>
-  </ul></p>
-  <h3> &nbsp; Refund</h3>
-</div>
-<div id="Contents">
-  <h2> <br>&nbsp; &nbsp;Refund</h2>
+function filter($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+?>
+
+<div id="Refund">
+  <h2>Refund</h2>
+  <form>
   <p>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; Receipt ID:</p>
   <p>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; UPC:</p>
   <p>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; Amount:</p>
-  <form> 
 	&nbsp;&nbsp;
 	<div id="LeftPanel"><p>&nbsp;&nbsp;Card Refund</p>
 	&nbsp;&nbsp;Card Type: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -38,24 +46,7 @@
 	&nbsp;&nbsp;Expiry Date: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
 	<input type="text" name="Expiry Date"/><br /><br />
 	<p align="right"><input type="submit" name="Next"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p></div>
-	<div id="RightPanel"><p>&nbsp;&nbsp;Cash Refund</p>
-	  <p><br />
-	    <br /><br />
-	    <br />
-	    <br />
-	  </p>
-	  <p align="right"><br />
-      </p>
-	  <p align="right"><br />
-	    <input type="submit" name="Next2"/>
-	    <br />
-      </p>
-	  <p align="right">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-	</div>
-	
-	
   </form>
 </div>
-<p>&nbsp;</p>
 </body>
 </html>
