11f55a02d758848958dd70f9505f0fe803970f45
rickywongwk <rickywong2323@gmail.com>
2015-04-06 20:20:08 -0700

    fixed issue when deploying to Google Cloud
    database was not connectable.
    Connect.php database was wrong.

--- UI/core/Connect.php
+++ UI/core/Connect.php
@@ -4,9 +4,10 @@
 		$sql = null;
 		if (isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
 			// Connect from App Engine.
-			$sql = new mysqli(null, 'root', '', 'amspassword', null, '/cloudsql/ams-cmpt354g6-2015spring:test');
+			$sql = new mysqli(null, 'root', '', 'AMS_CMPT354', null, '/cloudsql/ams-cmpt354g6-2015spring:test');
 		} else {
 			// Connect from a development environment.
+            //$sql = new mysqli('173.194.230.229:3306', 'nicholas', 'amspassword', 'AMS_CMPT354');
 			$sql = new mysqli('[2001:4860:4864:1:3208:ce09:5af3:c0cd]:3306', 'nicholas', 'amspassword', 'AMS_CMPT354');
 		}
 		return $sql;
