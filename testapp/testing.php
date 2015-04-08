a3f5252e001b9970ee58ddf75da3e275d09e6abc
rickywongwk <rickywong2323@gmail.com>
2015-04-08 15:01:43 -0700

    Removed redundant files

--- testapp/testing.php
+++
@@ -1,9 +0,0 @@
-// Using mysqli (connecting from App Engine)
-$sql = new mysqli(
-  null, // host
-  'root', // xy11235@gmail.com
-  '',     // 
-  '', // database name
-  null,
-  '/cloudsql/ams-cmpt354g6-2015spring:test'
-  );
